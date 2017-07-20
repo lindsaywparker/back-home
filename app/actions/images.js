9a804f3ffa04e899d146b76f1c1ab697e22291b3
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-07-20 14:56:12 -0600

    Reorder actions

--- app/actions/images.js
+++ app/actions/images.js
@@ -1,5 +1,12 @@
 import fetchImages from '../helpers/apiUtils';
 
+export const imagesFetchSuccess = (images) => {
+  return {
+    type: 'IMAGES_FETCH_SUCCESS',
+    images,
+  };
+};
+
 export const fetchImagesAction = () => {
   return (dispatch) => {
     return fetchImages()
@@ -9,9 +16,3 @@ export const fetchImagesAction = () => {
   };
 };
 
-export const imagesFetchSuccess = (images) => {
-  return {
-    type: 'IMAGES_FETCH_SUCCESS',
-    images,
-  };
-};
