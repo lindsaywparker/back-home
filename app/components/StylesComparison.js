4ed534dd6a940ba0aa90a1c80641f525ee943dae
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-07-25 17:10:10 -0600

    Styling and UX

--- app/components/StylesComparison.js
+++ app/components/StylesComparison.js
@@ -27,9 +27,10 @@ export default class StylesComparison extends Component {
     }, {});
 
     const content = this.props.isLoading ?
-      <img className='loader' src='../assets/loader.gif' alt='Loading...' /> :
+      <div className='loader-container'>
+        <img className='loader' src='../assets/loader.gif' alt='Loading...' />
+      </div> :
       <div>
-        <PageInfo location={location} />
         <div className='styles-matrix'>
           {STYLES.map((style, i) =>
             <ImageSlider key={i}
@@ -37,17 +38,22 @@ export default class StylesComparison extends Component {
                          style={style} />,
           )}
         </div>
+        <div className='comparison-footer'>
+          <h3>See more from</h3>
+          <div className='see-more-container'>
+            {CATEGORIES.map((category, i) =>
+              <NavLink to={`/styles/all/${category}`} key={i} className='see-more-category'>
+                {DISPLAY_NAME[category]}
+              </NavLink>,
+            )}
+          </div>
+        </div>
       </div>;
 
     return (
       <div>
+        <PageInfo location={location} />
         {content}
-        <h3>See more</h3>
-        {CATEGORIES.map((category, i) =>
-          <NavLink to={`/styles/all/${category}`} key={i} className='slider-title see-more-category'>
-            {DISPLAY_NAME[category]}
-          </NavLink>,
-        )}
       </div>
     );
   }
