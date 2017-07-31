79f2b7c0dfbecda06c6f86dd05833c339ce171b5
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-07-30 21:14:13 -0600

    WIP: add tagline, remove extras on buttons

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,15 +4,16 @@ import { NavLink } from 'react-router-dom';
 const Home = () => {
   return (
     <div className='home'>
+      <h1 className='tagline'>DISCOVER YOUR DESIGN STYLE</h1>
       <container className='homepage-btns-container'>
         <NavLink to='/styles'>
           <button className='homepage-btns styles'>
-            <h2 className='btn-title'>Compare Styles</h2>
+            <h4 className='btn-title'>Compare Styles</h4>
           </button>
         </NavLink>
         <NavLink to='/favorites'>
           <button className='homepage-btns favorites'>
-            <h2 className='btn-title'>View Favorites</h2>
+            <h4 className='btn-title'>View Favorites</h4>
           </button>
         </NavLink>
       </container>
